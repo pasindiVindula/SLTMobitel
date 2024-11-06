46e12b2b8725f042db486a7ca768b8d265230dc4
Pasindi Vindula <123557234+pasindiVindula@users.noreply.github.com>
2024-11-06 15:55:23 +0530

    slider center content done

--- src/app/components/Carosuel.tsx
+++ src/app/components/Carosuel.tsx
@@ -1,9 +1,19 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Button } from "@nextui-org/react";
 
 export interface ImageSliderType {
-  images: { title: string; url: string }[];
+  images: {
+    title: string;
+    url: string;
+    description?: string;
+    heading: string;
+    content: string;
+    label: string;
+    heading2: string;
+    content2: string;
+  }[];
 }
 
 export default function BannerSlider({ images }: ImageSliderType) {
@@ -20,86 +30,104 @@ export default function BannerSlider({ images }: ImageSliderType) {
 
   return (
     <Slider {...settings} className="banner-slider">
-      {images?.map((image, index) => (
-        <div key={index} data-testid="banner-slider-item">
+      {images.map((image, index) => (
+        <div key={index} data-testid="banner-slider-item" className="relative">
           <img
             alt={image.title}
             src={image.url}
-            className="h-auto w-full object-cover object-center"
+            className="h-auto w-full object-cover object-center bg-black bg-opacity-40"
           />
+          <div className="absolute inset-y-0 right-0 w-1/2 flex flex-col items-center justify-center text-center text-white p-4">
+            <div className="w-1/2 text-left space-y-6">
+              <h2 className="text-3xl font-bold">{image.heading}</h2>
+              <div>{image.content}</div>
+              {image.heading2 && (
+                <div>
+                  <h2 className="text-3xl font-bold">{image.heading2}</h2>
+                  <div>{image.content2}</div>
+                </div>
+              )}
+              {image.label && (
+                <Button radius="full" className="bg-[#1055A7] text-white">
+                  {image.label}
+                </Button>
+              )}
+            </div>
+          </div>
         </div>
       ))}
     </Slider>
   );
 }
 
-export function PrevArrow({ currentSlide,slideCount, ...props }: any) {
-    return (
-      <div
-        {...props}
-        data-testid="image-slider-pre-arrow-buttons-container"
-        className="slick-arrow absolute left-0 top-[40%] z-10 rotate-180 cursor-pointer md:left-4 lg:block"
+export function PrevArrow({ currentSlide, slideCount, ...props }: any) {
+  return (
+    <div
+      {...props}
+      data-testid="image-slider-pre-arrow-buttons-container"
+      className="slick-arrow absolute left-0 top-[40%] z-10 rotate-180 cursor-pointer md:left-4 lg:block"
+    >
+      {/* SVG for Previous Arrow */}
+      <svg
+        width="40"
+        height="41"
+        viewBox="0 0 40 41"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          width="40"
-          height="41"
-          viewBox="0 0 40 41"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            x="39.5"
-            y="1.3877"
-            width="39"
-            height="39"
-            rx="19.5"
-            transform="rotate(90 39.5 1.3877)"
-            stroke="none"
-            className="stroke-white hover:stroke-white"
-          />
-          <path
-            d="M20.4999 28.469L28.2906 20.6783M28.2906 20.6783L20.4999 12.8877M28.2906 20.6783H11.9999"
-            stroke="#E67200"
-            strokeLinecap="square"
-            className="stroke-white hover:stroke-white"
-          />
-        </svg>
-      </div>
-    );
-  }
-  
-  export function NextArrow({ currentSlide,slideCount, ...props }: any) { 
-    return (
-      <div
-        {...props}
-        data-testid="image-slider-next-arrow-buttons-container"
-        className="slick-arrow absolute right-0 top-[40%] cursor-pointer md:right-9 lg:block"
+        <rect
+          x="39.5"
+          y="1.3877"
+          width="39"
+          height="39"
+          rx="19.5"
+          transform="rotate(90 39.5 1.3877)"
+          stroke="none"
+          className="stroke-white hover:stroke-white"
+        />
+        <path
+          d="M20.4999 28.469L28.2906 20.6783M28.2906 20.6783L20.4999 12.8877M28.2906 20.6783H11.9999"
+          stroke="#E67200"
+          strokeLinecap="square"
+          className="stroke-white hover:stroke-white"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export function NextArrow({ currentSlide, slideCount, ...props }: any) {
+  return (
+    <div
+      {...props}
+      data-testid="image-slider-next-arrow-buttons-container"
+      className="slick-arrow absolute right-0 top-[40%] cursor-pointer md:right-9 lg:block"
+    >
+      {/* SVG for Next Arrow */}
+      <svg
+        width="40"
+        height="41"
+        viewBox="0 0 40 41"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          width="40"
-          height="41"
-          viewBox="0 0 40 41"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            x="39.5"
-            y="1.3877"
-            width="39"
-            height="39"
-            rx="19.5"
-            transform="rotate(90 39.5 1.3877)"
-            stroke="#E67200"
-            className="stroke-white hover:stroke-white"
-          />
-          <path
-            d="M20.4999 28.469L28.2906 20.6783M28.2906 20.6783L20.4999 12.8877M28.2906 20.6783H11.9999"
-            stroke="#E67200"
-            strokeLinecap="square"
-            className="stroke-white hover:stroke-white"
-          />
-        </svg>
-      </div>
-    );
-  }
-  
+        <rect
+          x="39.5"
+          y="1.3877"
+          width="39"
+          height="39"
+          rx="19.5"
+          transform="rotate(90 39.5 1.3877)"
+          stroke="#E67200"
+          className="stroke-white hover:stroke-white"
+        />
+        <path
+          d="M20.4999 28.469L28.2906 20.6783M28.2906 20.6783L20.4999 12.8877M28.2906 20.6783H11.9999"
+          stroke="#E67200"
+          strokeLinecap="square"
+          className="stroke-white hover:stroke-white"
+        />
+      </svg>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,66 @@
-'use client'
+"use client";
 
 import React from "react";
 import styles from "./home.module.css";
 import Navbar from "./components/Navbar";
-import DetailCard from "./components/card/details"
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const BannerSlider = dynamic(() => import('./components/Carosuel'), { ssr: false });
-
+const BannerSlider = dynamic(() => import("./components/Carosuel"), {
+  ssr: false,
+});
 
 export default function Home() {
   const images = [
     {
-      title: 'Image 1',
-      url: '/images/slider1.jpg',
+      title: "Image 1",
+      url: "/images/background.png",
+      content:
+        "Lörem ipsum laktigt vaska. Ott neresamma ifall fyning. Diheten pseudotion. Hotelljournalist ibel äns inte gåktiga i är.Preligen esk.",
+      heading: "Mobile Offers Banner",
+      label: "Explore More",
+      heading2: "",
+      content2: "",
     },
     {
-      title: 'Image 2',
-      url: '/images/slider2.png',
+      title: "Image 2",
+      url: "/images/slider2.png",
+      content:
+        "Lörem ipsum laktigt vaska. Ott neresamma ifall fyning. Diheten pseudotion. Hotelljournalist ibel äns inte gåktiga i är.Preligen esk.",
+      heading: "4G LTE Coverage",
+      label: "Coverage Map",
+      heading2: "",
+      content2: "",
     },
     {
-      title: 'Image 3',
-      url: '/images/slider3.png',
+      title: "Image 3",
+      url: "/images/slider3.png",
+      content:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.",
+      heading: "Welcome Aboard",
+      label: "Log In",
+      heading2: "",
+      content2: "",
     },
     {
-      title: 'Image 4',
-      url: '/images/slider4.png',
+      title: "Image 4",
+      url: "/images/slider5.png",
+      content:
+        "Lörem ipsum laktigt vaska. Ott neresamma ifall fyning. Diheten pseudotion. Hotelljournalist ibel äns inte gåktiga i är.Preligen esk.",
+      heading: "Map Locator",
+      label: "",
+      heading2: "",
+      content2: "",
     },
     {
-      title: 'Image 5',
-      url: '/images/slider5.png',
+      title: "Image 5",
+      url: "/images/slider6.jpg",
+      content:
+        "Lörem ipsum laktigt vaska. Ott neresamma ifall fyning. Diheten pseudotion. Hotelljournalist ibel äns inte gåktiga i är.Preligen esk.",
+      heading: "ENTERPRISE",
+      label: "Explore More",
+      heading2: "SMALL BUSINESS",
+      content2:
+        "Enhance the productivity and value of your business with the our latest and innovative ICT products and solutions",
     },
   ];
   return (
@@ -38,7 +69,6 @@ export default function Home() {
         <Navbar />
       </div>
       <div className={styles.content}>
-        <DetailCard />
         <div className="relative">
           <BannerSlider images={images} />
         </div>
